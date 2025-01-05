9a48262942c2754c77937e6b9f4b528754cf23a3
elisa <elisacss95@gmail.com>
2025-01-05 12:07:55 -0300

    Função de listar os membros do grupo

--- cardcaptor/src/main/java/com/senac/cardcaptor/controller/PhotocardController.java
+++ cardcaptor/src/main/java/com/senac/cardcaptor/controller/PhotocardController.java
@@ -134,11 +134,43 @@ public class PhotocardController {
     }
 
     //----------------------------------------------------------------Listas
-    
+    //Listar grupos
     @GetMapping("/grupos")
     public String listarGrupos(Model model) {
         model.addAttribute("listaGrupos", listaGrupos);
         return "listaGrupos";
     }
+
+    //Listar membros do grupo selecionado
+    @GetMapping("/grupos/{id}")
+    public String listarIdolsPorGrupo(@PathVariable Integer id, Model model) {
+        Grupo grupoSelecionado = null;
+
+        for (Grupo grupo : listaGrupos) {
+            if (grupo.getId().equals(id)) {
+                grupoSelecionado = grupo;
+                break;
+            }
+        }
+
+        List<Idol> idolsGrupoSelecionado = new ArrayList<>();
+
+        if (grupoSelecionado != null) {
+            for (Idol idol : listaIdols) {
+                if (idol.getGrupo().getId().equals(id)) {
+                    idolsGrupoSelecionado.add(idol);
+                }
+            }
+
+            model.addAttribute("grupo", grupoSelecionado);
+            model.addAttribute("listaIdols", idolsGrupoSelecionado);
+        } else {
+            model.addAttribute("grupo", null);
+            model.addAttribute("listaIdols", new ArrayList<>());
+        }
+
+        return "listaIdols";
+    }
+   
 
 }
