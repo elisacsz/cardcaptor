c6faa1dff6d5f9f82c2ba1dc61efc79a40497421
Elisa Souza <114261672+elisacsz@users.noreply.github.com>
2025-01-12 22:09:58 -0300

    Update PhotocardRepository.java

--- cardcaptor/src/main/java/com/senac/cardcaptor/repository/PhotocardRepository.java
+++ cardcaptor/src/main/java/com/senac/cardcaptor/repository/PhotocardRepository.java
@@ -1,6 +1,7 @@
 package com.senac.cardcaptor.repository;
 
 import com.senac.cardcaptor.model.Photocard;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
