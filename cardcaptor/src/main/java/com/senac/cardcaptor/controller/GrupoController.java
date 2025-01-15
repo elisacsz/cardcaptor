68f9cf97e83957fe77364b374bfeedb11c6fa4ea
elisa <elisacss95@gmail.com>
2025-01-15 13:09:49 -0300

    Funcionando Listagem de Grupos e Idols com BD

--- cardcaptor/src/main/java/com/senac/cardcaptor/controller/GrupoController.java
+++ cardcaptor/src/main/java/com/senac/cardcaptor/controller/GrupoController.java
@@ -1,21 +1,27 @@
 package com.senac.cardcaptor.controller;
 
+import com.senac.cardcaptor.model.Grupo;
+import com.senac.cardcaptor.service.GrupoService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author elisa
  */
-@RestController
+@Controller
 public class GrupoController {
-    
-    /*//Listar grupos
+
+    @Autowired
+    GrupoService grupoService;
+
     @GetMapping("/grupos")
-    public String listarGrupos(Model model) {
+    public String index(Model model) {
+        List<Grupo> listaGrupos = grupoService.listarGrupos();
         model.addAttribute("listaGrupos", listaGrupos);
         return "listaGrupos";
-    }*/
-    
+    }
 }
--- cardcaptor/src/main/java/com/senac/cardcaptor/controller/IdolController.java
+++ cardcaptor/src/main/java/com/senac/cardcaptor/controller/IdolController.java
@@ -2,48 +2,36 @@ package com.senac.cardcaptor.controller;
 
 import com.senac.cardcaptor.model.Grupo;
 import com.senac.cardcaptor.model.Idol;
-import java.util.ArrayList;
+import com.senac.cardcaptor.service.GrupoService;
+import com.senac.cardcaptor.service.IdolService;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author elisa
  */
-@RestController
+@Controller
 public class IdolController {
-    /*
-    //Listar membros do grupo selecionado
-    @GetMapping("/grupos/{id}")
-    public String listarIdolsPorGrupo(@PathVariable Integer id, Model model) {
-        Grupo grupoSelecionado = null;
 
-        for (Grupo grupo : listaGrupos) {
-            if (grupo.getId().equals(id)) {
-                grupoSelecionado = grupo;
-                break;
-            }
-        }
+    @Autowired
+    GrupoService grupoService;
 
-        List<Idol> idolsGrupoSelecionado = new ArrayList<>();
+    @Autowired
+    IdolService idolService;
 
-        if (grupoSelecionado != null) {
-            for (Idol idol : listaIdols) {
-                if (idol.getGrupo().getId().equals(id)) {
-                    idolsGrupoSelecionado.add(idol);
-                }
-            }
+    @GetMapping("/grupos/{grupoId}/idols")
+    public String listarIdolsDoGrupo(@PathVariable Integer grupoId, Model model) {
+        Grupo grupo = grupoService.buscarGrupoPorId(grupoId);
 
-            model.addAttribute("grupo", grupoSelecionado);
-            model.addAttribute("listaIdols", idolsGrupoSelecionado);
-        } else {
-            model.addAttribute("grupo", null);
-            model.addAttribute("listaIdols", new ArrayList<>());
-        }
+        List<Idol> listaIdols = idolService.listarIdolsPorGrupo(grupoId);
 
+        model.addAttribute("grupo", grupo);
+        model.addAttribute("listaIdols", listaIdols);
         return "listaIdols";
-    }*/
+    }
 }
